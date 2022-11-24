61cc8cdc35a7e4436173237c7a81f94bc4d1efb8
Sally Azulay <sallyazulay@Sallys-Mac-mini.local>
2022-11-24 16:58:20 +0200

    adding stack navigation

---
+++ App.js
@@ -0,0 +1,28 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {createStackNavigator} from '@react-navigation/stack';
+import ChatListScreen from './screens/ChatListScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="ChatList" component={ChatListScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default App;
---
+++ screens/ChatListScreen.js
@@ -0,0 +1,21 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const ChatListScreen = () => {
+  return (
+    <View>
+      <Text>ChatListScreens</Text>
+      <Text>ChatListScreens</Text>
+      <Text>ChatListScreens</Text>
+      <Text>ChatListScreens</Text>
+      <Text>ChatListScreens</Text>
+      <Text>ChatListScreens</Text>
+      <Text>ChatListScreens</Text>
+      <Text>ChatListScreens</Text>
+      <Text>ChatListScreens</Text>
+      <Text>ChatListScreens</Text>
+    </View>
+  );
+};
+
+export default ChatListScreen;
