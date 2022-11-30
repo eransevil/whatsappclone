0d820f94e45301c3bbe15176b2a14320924eca68
erans <eran.sevil@xtrade.com>
2022-11-30 03:50:04 -0800

    comment

--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -1,6 +1,7 @@
 import {View, Text, StyleSheet, Button} from 'react-native';
 import React from 'react';
-
+//
+//
 const ChatListScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
